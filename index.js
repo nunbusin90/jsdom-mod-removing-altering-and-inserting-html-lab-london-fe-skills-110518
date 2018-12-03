5e920e43cd7148f89209146ababdedf072387e13
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-03 20:56:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+var element = document.createElement('h1');
+element.id = 'victory';
+element.innerHTML = 'Nayeon';
+
+document.body.appendChild(element);
+element.style.textAlign='center';
+
+victory.remove();
