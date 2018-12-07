592288b05a2b117234c59694efb2f5b2dc44ac02
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-07 15:37:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
-
+  main.remove()
+  
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+  var newHeader = document.createElement('h1');
+  newHeader.id='victory';
+  newHeader.innerText='Nayeon is the champion';
