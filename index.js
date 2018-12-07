dc00a55ceebf2084126678be4f8c39467eb7a16a
NA YEON LEE <lee.nayeon90@gmail.com>
2018-12-07 15:08:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
-
+  main.remove()
+  
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+  var element = document.createElement('h1');
+  element.id = 'victory';
+  element.innerHTML = 'Nayeon is champion!';
+  
+  document.body.appendChild(element);
